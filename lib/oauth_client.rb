60092345ec2534294f057f84a9afaee0c98def68
Ke Xu <leonhartx.k@gmail.com>
2016-05-15 23:42:59 +0900

    remove debug code

--- lib/oauth_client.rb
+++ lib/oauth_client.rb
@@ -1,6 +1,5 @@
 class OauthClient
   def initialize user, klass = nil
-  	binding.pry
     klass ||= "#{user.provider}".capitalize + "Client"
     klazz = Object.const_get klass
     @proxy = klazz.new user
