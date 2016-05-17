424e40dbc3d8dbf7d740431d86cb083015b93e06
Ke Xu <leonhartx.k@gmail.com>
2016-05-17 16:32:01 +0900

    add search index to support paginate

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,5 +1,11 @@
 class SearchsController < ApplicationController
   def create
-    @events = Event.where("title like '%" + params[:query] + "%'")
+    session[:query] = params[:query]
+    redirect_to searchs_path
+  end
+
+  def index
+    query = session[:query] || ""
+    @searchs = Event.where("title like '%" + query + "%'").paginate(page: params[:page], per_page: 5)
   end
 end
--- spec/controllers/searchs_controller_spec.rb
+++ spec/controllers/searchs_controller_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe SearchsController, :type => :controller do
+  describe "Get index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe "search" do
     it "returns http success" do
       post :create, params: { query: "event" }
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:found)
     end
   end
 end
